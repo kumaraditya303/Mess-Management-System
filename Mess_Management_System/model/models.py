acc98bbfe6e4a850f59dc9338dd6d679a08a2565
Kumar Aditya <rahuladitya303@gmail.com>
2020-06-09 16:42:48 +0530

    Updated database model for admin security

--- Mess_Management_System/model/models.py
+++ Mess_Management_System/model/models.py
@@ -15,15 +15,15 @@ class User(UserMixin, db.Model):
     avatar = db.Column(db.Text, nullable=False,
                        default=Gravatar(str(email)).get_image())
     balance = db.Column(db.Float, nullable=True, default=0.0)
-    order_history = db.Column(db.JSON, nullable=True,default='')
+    order_history = db.Column(db.JSON, nullable=True, default='')
     total_balance = db.Column(db.Float, nullable=True, default=0.0)
-    password = db.Column(db.Text, nullable=True, unique=False, default=None)
+    password = db.Column(db.Text, nullable=True, unique=False)
 
 
 class Dishes(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True, nullable=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
-    picture = db.Column(db.LargeBinary, unique=False, nullable=False)
+    picture = db.Column(db.LargeBinary, nullable=False)
     price = db.Column(db.Float, nullable=False, default=0.0)
     description = db.Column(db.Text, unique=False, nullable=False)
     cook_time = db.Column(db.Interval, unique=False, nullable=False)
